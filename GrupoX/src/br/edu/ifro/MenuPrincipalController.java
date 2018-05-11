de24492d0574ce6b6dc68725b909742da198c028
97598399204 <matheusfernandesluz@gmail.com>
2018-05-11 08:47:20 -0400

    criando tela cadastro de alunos

--- GrupoX/src/br/edu/ifro/MenuPrincipalController.java
+++ GrupoX/src/br/edu/ifro/MenuPrincipalController.java
@@ -7,12 +7,18 @@ package br.edu.ifro;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -29,6 +35,10 @@ public class MenuPrincipalController implements Initializable {
     private TextField txtNum1;
     @FXML
     private JFXTextField txtResultado;
+    @FXML
+    private MenuItem menuFechar;
+    @FXML
+    private JFXButton btnFechar;
 
     /**
      * Initializes the controller class.
@@ -45,6 +55,28 @@ public class MenuPrincipalController implements Initializable {
         
         Double resultado = num1 + num2;
         txtResultado.setText(resultado.toString());
+    }
+
+    @FXML
+    private void abrirCadastroAluno(ActionEvent event) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(),900,682);
+            Stage stage = new Stage();
+            stage.setTitle("Cadastrar Aluno");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(IOException e){
+
+}
+    }
+
+    @FXML
+    private void Fechar(ActionEvent event) {
+        Stage stage = (Stage) btnFechar.getScene().getWindow();
+        stage.close();
     }
     
 }
